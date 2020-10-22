f147e86278b2a6688f693f6c25c288c44b9128d5
Cathrine Kiel Skovbjerg <42338141+cks2903@users.noreply.github.com>
2020-10-22 16:12:02 +0200

    Add files via upload

--- EMMA200EMMAX+FarmCPU/GWAS_emmax_emma200/EMMA200Emmaxinput.R
+++ EMMA200EMMAX+FarmCPU/GWAS_emmax_emma200/EMMA200Emmaxinput.R
@@ -1,4 +1,4 @@
-geno=read.table("/home/cks/norfab/faststorage/Genotype_data_ProFaba_20201002/SNPsWithNoMissingness_OCT2020/GenericGenotypeFile.csv",head=T,sep=",")
+geno=read.table("../GenericGenotypeFile.csv",head=T,sep=",")
 
 geno_new=geno[,4:ncol(geno)]
 geno_new[geno_new=="1"]<-2
@@ -7,4 +7,10 @@ geno_new[geno_new=="-1"]<-0
 
 geno_new_new=cbind(geno[,2:3],geno_new)
 colnames(geno_new_new)=c("# CHROM","POS",colnames(geno)[4:ncol(geno)])
+
 write.table(geno_new_new,"GenericGenotypeFile_input.csv",quote=F,row.names=F,col.names=T,sep=",")
+# if we don't have integers as position emma200+emmax python implementation do not work
+if (length(which(geno_new_new$POS%%1!=0))>1){
+	geno_new_new$POS=round(geno_new_new$POS)
+	write.table(geno_new_new,"GenericGenotypeFile_input.csv",quote=F,row.names=F,col.names=T,sep=",")
+}
--- EMMA200EMMAX+FarmCPU/GWAS_emmax_emma200/RUN_EMMA200EMMAX200.sh
+++ EMMA200EMMAX+FarmCPU/GWAS_emmax_emma200/RUN_EMMA200EMMAX200.sh
@@ -2,5 +2,5 @@ conda deactivate
 source ~/miniconda2/etc/profile.d/conda.sh
 conda activate myproject
 
-python /home/cks/NChain/faststorage/GWASimplementation/atgwas/src/gwa.py -o "" -a emmax -m 8 -r FN_acidity_phenotypes_avg.csv -f Fakephenotypes.csv --data_format="diploid_int"
+python /home/cks/NChain/faststorage/GWASimplementation/atgwas/src/gwa.py -o "" -a emmax -m 8 -r Fakephenotypes.csv -f GenericGenotypeFile_input.csv --data_format="diploid_int"
 # IF MAF=0.05 x/(156)=0.05 <=> x= 7.8		MAC=8 (because apparently this is calculated on a haploid level)
